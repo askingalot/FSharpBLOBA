8e83ecc030d56ecd8483f011c915d404a78e13af
Andy Collins <askingalot@gmail.com>
2015-07-26 11:09:22 -0500

    Fixing compiler error that I should have never commited

---
+++ Repository/Ticket.fs
@@ -0,0 +1,12 @@
+﻿namespace Repository
+
+open System
+open Model
+
+module Tickets =
+    let getAll () = 
+        Seq.ofList[
+            {Id = 1; Summary = ShortSummary "Something went wrong"; Details = "I was doing something and then this other thing happened and now it's broke"; OccurrenceDateTime = DateTime.Now; Priority = High; IsResolved = false; ResolutionDateTime = None };
+            {Id = 2; Summary = ShortSummary "Something went wrong"; Details = "I was doing something and then this other thing happened and now it's broke"; OccurrenceDateTime = DateTime.Now; Priority = High; IsResolved = false; ResolutionDateTime = None };
+            {Id = 3; Summary = ShortSummary "Something went wrong"; Details = "I was doing something and then this other thing happened and now it's broke"; OccurrenceDateTime = DateTime.Now; Priority = High; IsResolved = false; ResolutionDateTime = None };
+        ]
